0b0432623d9167a555d92bbb1c908ccbeab38ac9
Sony AK <sony@sony-ak.com>
2025-04-19 00:33:43 +0700

    Enhance project setup with Prettier and Tailwind CSS plugins; update globals.css for improved font handling and layout in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,117 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="[--pattern-fg:var(--color-gray-950)]/5 dark:[--pattern-fg:var(--color-white)]/10 relative grid min-h-screen grid-cols-[1fr_2.5rem_auto_2.5rem_1fr] grid-rows-[1fr_1px_auto_1px_1fr] bg-white dark:bg-gray-950">
+      <div className="col-start-3 row-start-3 flex max-w-lg flex-col bg-gray-100 p-2 dark:bg-white/10">
+        <div className="rounded-xl bg-white p-10 text-sm/7 text-gray-700 dark:bg-gray-950 dark:text-gray-300">
+          <div className="text-2xl font-bold">example.com</div>
+          <div className="space-y-6">
+            <p className="mt-1.5 leading-relaxed">
+              Quick description about the domain. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit.
+            </p>
+            <ul className="space-y-3">
+              <li className="flex">
+                <svg
+                  className="w-5.5 h-[1lh] shrink-0"
+                  viewBox="0 0 22 22"
+                  fill="none"
+                  strokeLinecap="square"
+                >
+                  <circle cx="11" cy="11" r="11" className="fill-sky-400/25" />
+                  <circle
+                    cx="11"
+                    cy="11"
+                    r="10.5"
+                    className="stroke-sky-400/25"
+                  />
+                  <path
+                    d="M8 11.5L10.5 14L14 8"
+                    className="stroke-sky-800 dark:stroke-sky-300"
+                  />
+                </svg>
+                <p className="ml-3">
+                  Price{" "}
+                  <code className="font-mono font-medium text-gray-950 dark:text-white">
+                    USD 00.000
+                  </code>{" "}
+                  or{" "}
+                  <code className="font-mono font-medium text-gray-950 dark:text-white">
+                    IDR 000.000.000
+                  </code>
+                  .
+                </p>
+              </li>
+              <li className="flex">
+                <svg
+                  className="w-5.5 h-[1lh] shrink-0"
+                  viewBox="0 0 22 22"
+                  fill="none"
+                  strokeLinecap="square"
+                >
+                  <circle cx="11" cy="11" r="11" className="fill-sky-400/25" />
+                  <circle
+                    cx="11"
+                    cy="11"
+                    r="10.5"
+                    className="stroke-sky-400/25"
+                  />
+                  <path
+                    d="M8 11.5L10.5 14L14 8"
+                    className="stroke-sky-800 dark:stroke-sky-300"
+                  />
+                </svg>
+                <p className="ml-3">Full support during domain transfer.</p>
+              </li>
+              <li className="flex">
+                <svg
+                  className="w-5.5 h-[1lh] shrink-0"
+                  viewBox="0 0 22 22"
+                  fill="none"
+                  strokeLinecap="square"
+                >
+                  <circle cx="11" cy="11" r="11" className="fill-sky-400/25" />
+                  <circle
+                    cx="11"
+                    cy="11"
+                    r="10.5"
+                    className="stroke-sky-400/25"
+                  />
+                  <path
+                    d="M8 11.5L10.5 14L14 8"
+                    className="stroke-sky-800 dark:stroke-sky-300"
+                  />
+                </svg>
+                <p className="ml-3">Good investment.</p>
+              </li>
+            </ul>
+            <p>
+              If you interested please contact Name at{" "}
+              <code className="font-mono font-medium text-gray-950 dark:text-white">
+                <a
+                  href="https://example.com.com"
+                  className="underline-offset-3 text-gray-950 underline decoration-sky-400 hover:decoration-2 dark:text-white"
+                >
+                  user@example.com
+                </a>
+              </code>
+            </p>
+          </div>
+          <hr className="border-(--pattern-fg) my-6 w-full" />
+          <p className="mb-3">Other domain for sale:</p>
+          <p className="font-semibold">
+            <a
+              href="https://example.com"
+              className="underline-offset-3 text-gray-950 underline decoration-sky-400 hover:decoration-2 dark:text-white"
+            >
+              example.com
+            </a>
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="border-x-(--pattern-fg) relative -right-px col-start-2 row-span-full row-start-1 border-x bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed"></div>
+      <div className="border-x-(--pattern-fg) relative -left-px col-start-4 row-span-full row-start-1 border-x bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed"></div>
+      <div className="bg-(--pattern-fg) relative -bottom-px col-span-full col-start-1 row-start-2 h-px"></div>
+      <div className="bg-(--pattern-fg) relative -top-px col-span-full col-start-1 row-start-4 h-px"></div>
     </div>
   );
 }
