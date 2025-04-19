4e9e893bf24d18140057e87d602f327e82be6bc6
Sony AK <sony@sony-ak.com>
2025-04-19 22:39:22 +0700

    add bolabanget.com and bolabanget.id

---
+++ domains-for-sale/bolabanget.com/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "bolabanget.com for sale",
+  description: "Domain bolabanget.com for sale. Good for sports news, sports community etc. Targeting huge Indonesian and international market.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
